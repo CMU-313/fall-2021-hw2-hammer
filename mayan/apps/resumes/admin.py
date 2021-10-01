80cc9ee8a12b61b9840e30b828866dd55605a5ce
Michelle Li <mli6@andrew.cmu.edu>
2021-10-01 04:02:07 -0400

    cosmetic changes

--- mayan/apps/resumes/admin.py
+++ mayan/apps/resumes/admin.py
@@ -6,5 +6,5 @@ from .models import Resume
 @admin.register(Resume)
 class TagAdmin(admin.ModelAdmin):
     # filter_horizontal = ('documents',)
-    list_display = ('first_name', 'last_name', 'applicant_id', 'reviewer_first_name', 'reviewer_last_name', 'education', 'work', 'extracurriculars', 'skills_and_awards', 'comments') # TODO: add new form field names here
+    list_display = ('applicant_id', 'reviewer_first_name', 'reviewer_last_name', 'education', 'work', 'extracurriculars', 'skills_and_awards', 'comments') # TODO: add new form field names here
 
